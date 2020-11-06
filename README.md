b7ec30d6b8a1a8e262fbb568c91820b73cdd75f4
ramakrishna578 <ramakrishna578@gmail.com>
2020-11-06 21:36:34 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Description
+
+This projects depicts about my experience.
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Running the app
+
+```bash
+$ npm run watch
---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+// $(document).ready(() => {
+//   setTimeout(() => {
+//     var firstnameLength = $(".firstname").length;
+//     $($(".firstname").get()).each((i, e) => {
+//       $(e).delay(500 * (i - 1)).fadeIn(500);
+//       if(i >= firstnameLength - 1) {
+//         $(e).animate({
+//           "margin-right": "25px"
+//         });
+//       }
+//     });
+//   }, 1000);
+// });
